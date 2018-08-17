f6acd7f1e8b997af1598ca60e1774abfc732711b
Austin <alaw989@gmail.com>
2018-08-16 22:22:38 -0400

    added city and state

--- src/App.js
+++ src/App.js
@@ -1,53 +1,71 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
-// const ip = "http://ip-api.com/json";
+const ip = "http://ip-api.com/json";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weather: ""
+      weather: "",
+      city: "",
+      state: ""
     };
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(success);
+
     let currentComponent = this;
-    function success(position) {
-      const url = `https://api.darksky.net/forecast/6c2e47ef2096d8cbfc738b0df8ff7917/${
-        position.coords.latitude
-      },${position.coords.longitude}`;
-      console.log(position);
-      return axios(url, {
-        method: "GET",
-        mode: "no-cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json"
+
+    axios.get(ip)
+      .then(response => {
+
+        navigator.geolocation.getCurrentPosition(success);
+
+        const city = response.data.city;
+        const state = response.data.regionName;
+        console.log(response.data);
+
+        function success(position) {
+          const url = `https://api.darksky.net/forecast/6c2e47ef2096d8cbfc738b0df8ff7917/${
+            position.coords.latitude
+            },${position.coords.longitude}`;
+
+          console.log(position);
+
+          return axios(url, {
+            method: "GET",
+            mode: "no-cors",
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json"
+            }
+          }).then(response => {
+            const weatherData = response.data;
+            console.log(weatherData);
+            currentComponent.setState({
+              weather: weatherData.currently.apparentTemperature,
+              city: city,
+              state: state
+            });
+          });
         }
-      }).then(response => {
-        const weatherData = response.data;
-        console.log(weatherData);
-        currentComponent.setState({
-          weather: weatherData.currently.apparentTemperature
-        });
+      })
+      .catch(error => {
+        console.log(error);
       });
-    }
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Temperature: {this.state.weather} </h1>{" "}
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Your Location: {this.state.city}, {this.state.state}</h1>
+          <h2 className="App-title">Temperature: {this.state.weather} </h2>{" "}
         </header>{" "}
-        <p className="App-intro">
-          To get started, edit <code> src / App.js </code> and save to reload.{" "}
-        </p>{" "}
       </div>
     );
   }
